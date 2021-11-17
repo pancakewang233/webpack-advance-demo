c4f4dbe7520d71ceddab79588736f576f9761ce6
pancake <834430775@qq.com>
2021-11-17 22:24:57 +0800

    支持jsx文件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import {a} from './a' // 直接引入
 const b = import('./b') // 动态引入，也叫按需引入
+import {JsxDemo} from './jsx-demo'
 
+console.log(JsxDemo)
 const hi = () =>{
   console.log('pank')
   console.log(a)
---
+++ src/jsx-demo.jsx
@@ -0,0 +1,7 @@
+const JsxDemo = () => {
+  return (
+    <div>hi,jsxDemo</div>
+  )
+}
+
+export {JsxDemo}
