083977d1770a903217aacb7899942924ae9a9412
pancake <834430775@qq.com>
2021-11-18 21:27:31 +0800

    支持TS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import {a} from './a' // 直接引入
 import {JsxDemo} from './jsx-demo'
+import {x} from './ts-demo'
 const b = import('./b') // 动态引入，也叫按需引入
 
 console.log(JsxDemo)
+console.log(x)
 const hi = () =>{
   console.log('pank')
   console.log(a)
--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,25 @@ const ESLintWebpackPlugin = require('eslint-webpack-plugin')
 module.exports = {
   mode: 'production',
   resolve: {
-    extensions: ['.ts', '.js', '.jsx'],
+    extensions: ['.ts', '.js', '.jsx','.tsx'],
   },
   plugins: [new ESLintWebpackPlugin({
     // 增加什么后缀就检查什么文件
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
   })],
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.[jt]sx?$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [['@babel/preset-env'],['@babel/preset-react', {runtime: 'classic'}]]
+            presets: [
+              ['@babel/preset-env'],
+              ['@babel/preset-react', {runtime: 'classic'}],
+              ['@babel/preset-typescript']
+            ]
           }
         }
       }
