4940e4cec1ca5b05c0f6666286b857b2724eb37c
pancake <834430775@qq.com>
2021-11-18 22:08:17 +0800

    支持 TSX

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import {a} from './a' // 直接引入
 import {JsxDemo} from './jsx-demo'
 import {x} from './ts-demo'
+import {TsxDemo} from './tsx-demo'
+
 const b = import('./b') // 动态引入，也叫按需引入
 
+console.log(TsxDemo)
 console.log(JsxDemo)
 console.log(x)
 const hi = () =>{
