21d0553d66575e3a0373d2865e24e9720db97295
pancake <834430775@qq.com>
2021-11-18 21:59:29 +0800

    ESLint 提示 TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,17 @@ module.exports = {
     // 'react/jsx-users-react': [2],
     // 提示要在 jsx 里手动引入 React
     'react/react-in-jsx-scope': [2]
-  }
+  },
+  overrides: [{
+    files: ['*.ts', '*.tsx'],
+    parserOptions: {
+      project: './tsconfig.json'
+    },
+    extends: ['airbnb-typescript'],
+    rules: {
+      '@/typescript-eslint/object-curly-spacing': [0],
+      'import/prefer-default-export': [0]
+    }
+  }]
+
 }
